1760113fdafc58719e33ccddf2ca9577ef9bf0dd
Peter <petchar@ut.utm.edu>
2021-06-10 22:18:12 -0400

    Logging
    Logging functionality added to main and endpoint requests.

--- exercises/python-helloworld/app.py
+++ exercises/python-helloworld/app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import json
+import logging
 app = Flask(__name__)
 
 @app.route("/status")
@@ -9,7 +10,8 @@ def status():
         status=200,
         mimetype='application/json'
     )
-
+    ##Log Line
+    app.logger.info('Status request successful')
     return response
 
 @app.route("/metrics")
@@ -19,12 +21,17 @@ def metrics():
         status=200,
         mimetype='application/json'
     )
-
+    ##Log Line
+    app.logger.info('Metrics request successful')
     return response
 
 @app.route("/")
 def hello():
+    ##Log line
+    app.logger.info('Main request successful')
     return "Hello World!"
 
 if __name__ == "__main__":
+    ## stream logs to app.log file
+    logging.basicConfig(filename='app.log',level=logging.DEBUG)
     app.run(host='0.0.0.0')
